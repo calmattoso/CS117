eda912c138b188a19f1a8a99bd3dda44ef6d7463
Carlos Mattoso <calmattoso@hotmail.com>
2015-04-01 21:13:11 -0700

    (+) Templates for each image manipulation function.

---
+++ HW1/rotate_image.m
@@ -0,0 +1,15 @@
+function Irot = rotate_image(I,angle)
+ % function Irot = rotate_image(I,angle)
+ % 
+ %   This function takes an image I and creates a new version of the image
+ %   which is rotated by amount angle
+ %
+ % arguments:
+ %
+ %   I - the original grayscale image, stored as a matrix
+ %   angle - the amount by which to rotate the image
+ %
+ % return value:
+ %   
+ %   Irot - an image which containing the rotated original
+ %
---
+++ HW1/straighten.m
@@ -0,0 +1,6 @@
+% STRAIGHTEN.m
+%
+% This script loads in an image, allows the user to click on two points in 
+%   the image, and then rotates the image so that the line connecting 
+%   these two points is horizontal.
+%
